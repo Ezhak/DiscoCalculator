0ab3ac1ff3713e42d4143ec41ad1c1499a3392c4
Matias <matiasquintana.soria@hotmail.com>
2020-05-26 15:21:55 -0300

    Update Calculadora Disco.cpp

--- Calculadora Disco.cpp
+++ Calculadora Disco.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
 Calcula, en base a un cuadro de datos, el Seek Time, el Search Time y finalmente el Access Time.
 */
 #include <iostream>
@@ -20,7 +20,7 @@ int main(){
 	int sectorQueCai = 0; int caraEquivalencia = 0; int sectorEquivalencia = 0; int caraEquivalenciaVoy = 0; int sectorEquivalenciaVoy = 0;
 	int sectoresMoverse = 0; int sectorObjetivoAuxiliar = 0; float vueltaSector = 0;
 // Access Time = Seek Time + Search Time
-
+	setColor(COLORES::LIGHTMAGENTA);
 	for (int i = 2; i < 60; i++) {
 		gotoxy(i, 1); printf("%c", 205);
 		gotoxy(i, 30); printf("%c", 205);
@@ -33,7 +33,7 @@ int main(){
 	gotoxy(1, 30); printf("%c", 200);
 	gotoxy(60, 1); printf("%c", 187);
 	gotoxy(60, 30); printf("%c", 188);
-	gotoxy(2, 2);
+	gotoxy(2, 2); setColor(COLORES::BLUE);
 	cout << "Ingrese las RPM: " << endl;gotoxy(19, 2);cin >> rpm;
 	gotoxy(2, 3);
 	cout << "Ingrese las Caras Utilizables: " << endl;gotoxy(33, 3);cin >> carasUtilizables;
@@ -44,13 +44,13 @@ int main(){
 	gotoxy(2, 6);
 	cout << "Ingrese el Paso de Cilindro: " << endl;gotoxy(31, 6); cin >> pasoDeCilindro;
 	gotoxy(2, 7);
-	cout << "------------------------------" << endl;
+	setColor(COLORES::YELLOW); cout << "------------------------------" << endl; setColor(COLORES::BLUE);
 	gotoxy(2, 8);
 	cout << "Ingrese el punto actual: " << endl; gotoxy(27, 8); cin >> puntoActual;
 	gotoxy(2, 9);
 	cout << "Ingrese la peticion: " << endl; gotoxy(23, 9); cin >> peticion;
 	gotoxy(2, 10);
-	cout << "------------------------------" << endl;
+	setColor(COLORES::YELLOW); cout << "------------------------------" << endl; setColor(COLORES::BLUE);
 // Calculo de Seek Time - Se debe calcular los sectores por cilindro
 	sectoresPorCilindro = carasUtilizables * sectoresPorPista;
 // Calculo de cilindro y sector correspondientes al punto actual y al punto objetivo
@@ -69,7 +69,9 @@ int main(){
 // Calculo de SeekTime
 	seekTime = restaAuxiliar * pasoDeCilindro;
 	gotoxy(61, 2);
+	setColor(COLORES::LIGHTRED);
 	cout << "El SeekTime es de: " << fixed << setprecision(2) << seekTime << "ms" << endl;
+	setColor(COLORES::BLUE);
 // Calculo de SearchTime
 	vueltaPorMs = msAuxiliar / rpm;
 	gotoxy(61, 3);
@@ -121,11 +123,39 @@ int main(){
 // Calculo de Search Time
 	searchTime = sectoresMoverse * vueltaSector;
 	gotoxy(61, 11);
+	setColor(COLORES::LIGHTRED);
 	cout << "El SearchTime es de: " << fixed << setprecision(2) << searchTime << " ms" << endl;
+	setColor(COLORES::BLUE);
 // Calculo de AccesTime
 	accesTime = seekTime + searchTime;
 	gotoxy(61, 12);
+	setColor(COLORES::LIGHTRED);
 	cout << "El Access Time es de: " << fixed << setprecision(2) << accesTime << " ms" << endl;
+	setColor(COLORES::BLUE);
+	gotoxy(61, 14);
+	gotoxy(61, 15);
+	int iterator = 1;
+	while(iterator!=0) {
+	gotoxy(61, 16); setColor(COLORES::MAGENTA); cout << " 8888888888";cout << "          888" << "               888 " << endl;
+	gotoxy(61, 17); setColor(COLORES::MAGENTA); cout << " 888";cout << "                 888" << "               888 " << endl;
+	gotoxy(61, 18); setColor(COLORES::MAGENTA); cout << " 888";cout << "                 888" << "               888 " << endl;
+	gotoxy(61, 19); setColor(COLORES::MAGENTA); cout << " 8888888"; setColor(COLORES::YELLOW); cout << "    88888888"; setColor(COLORES::MAGENTA); cout << " 88888b." << "   "; setColor(COLORES::YELLOW); cout << "8888b."; setColor(COLORES::MAGENTA); cout<< " " << " 888" << "  888 " << endl;
+	gotoxy(61, 20); setColor(COLORES::MAGENTA); cout << " 888"; setColor(COLORES::YELLOW); cout << "           d88P"; setColor(COLORES::MAGENTA); cout << "  888 " << "'88b     "; setColor(COLORES::YELLOW); cout << "'88b"; setColor(COLORES::MAGENTA); cout<< " 888 " << ".88P " << endl;
+	gotoxy(61, 21); setColor(COLORES::MAGENTA); cout << " 888"; setColor(COLORES::YELLOW); cout << "          d88P"; setColor(COLORES::MAGENTA); cout << "   888" << "  888"; setColor(COLORES::YELLOW); cout << " .d888888 "; setColor(COLORES::MAGENTA); cout<< "888888K " << endl;
+	gotoxy(61, 22); setColor(COLORES::MAGENTA); cout << " 888"; setColor(COLORES::YELLOW); cout << "         d88P"; setColor(COLORES::MAGENTA); cout << "    888" << "  888"; setColor(COLORES::YELLOW); cout << " 888" << "  888"; setColor(COLORES::MAGENTA); cout<< " 888 " << "'88b" << endl;
+	gotoxy(61, 23); setColor(COLORES::MAGENTA); cout << " 8888888888"; setColor(COLORES::YELLOW); cout << " 88888888";; setColor(COLORES::MAGENTA); cout << " 888" << "  888 "; setColor(COLORES::YELLOW); cout << "'Y888888"; setColor(COLORES::MAGENTA); cout<< " 888" << "  888" << endl;
+	
+	gotoxy(61, 16); setColor(COLORES::YELLOW); cout << " 8888888888"; cout << "          888" << "               888 " << endl;
+	gotoxy(61, 17); setColor(COLORES::YELLOW); cout << " 888"; cout << "                 888" << "               888 " << endl;
+	gotoxy(61, 18); setColor(COLORES::YELLOW); cout << " 888"; cout << "                 888" << "               888 " << endl;
+	gotoxy(61, 19); setColor(COLORES::YELLOW); cout << " 8888888"; setColor(COLORES::MAGENTA); cout << "    88888888"; setColor(COLORES::YELLOW); cout << " 88888b." << "   "; setColor(COLORES::MAGENTA); cout << "8888b."; setColor(COLORES::YELLOW); cout << " " << " 888" << "  888 " << endl;
+	gotoxy(61, 20); setColor(COLORES::YELLOW); cout << " 888"; setColor(COLORES::MAGENTA); cout << "           d88P"; setColor(COLORES::YELLOW); cout << "  888 " << "'88b     "; setColor(COLORES::MAGENTA); cout << "'88b"; setColor(COLORES::YELLOW); cout << " 888 " << ".88P " << endl;
+	gotoxy(61, 21); setColor(COLORES::YELLOW); cout << " 888"; setColor(COLORES::MAGENTA); cout << "          d88P"; setColor(COLORES::YELLOW); cout << "   888" << "  888"; setColor(COLORES::MAGENTA); cout << " .d888888 "; setColor(COLORES::YELLOW); cout << "888888K " << endl;
+	gotoxy(61, 22); setColor(COLORES::YELLOW); cout << " 888"; setColor(COLORES::MAGENTA); cout << "         d88P"; setColor(COLORES::YELLOW); cout << "    888" << "  888"; setColor(COLORES::MAGENTA); cout << " 888" << "  888"; setColor(COLORES::YELLOW); cout << " 888 " << "'88b" << endl;
+	gotoxy(61, 23); setColor(COLORES::YELLOW); cout << " 8888888888"; setColor(COLORES::MAGENTA); cout << " 88888888";; setColor(COLORES::YELLOW); cout << " 888" << "  888 "; setColor(COLORES::MAGENTA); cout << "'Y888888"; setColor(COLORES::YELLOW); cout << " 888" << "  888" << endl;
+
+	}
+	gotoxy(61, 24);
 	anykey();
 	return 0;
 }
